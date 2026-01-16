22de5f5fe0e13e3b8d50240ce2d2254136539c7c
thoung168 <chaovanthoung168@gmail.com>
2026-01-17 03:01:34 +0700

    Remove spring-authorization-server folder

--- spring-authorization-server/src/main/java/auth/res_server/demo/dto/authority/ResponseAuthority.java
+++
@@ -1,9 +0,0 @@
-package auth.res_server.demo.dto.authority;
-
-import lombok.Builder;
-
-@Builder
-public record ResponseAuthority(
-        String name,
-        String description
-) {}
--- spring-authorization-server/src/main/java/auth/res_server/demo/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package auth.res_server.demo.service;
-
-public interface EmailService {
-
-    void sendVerificationEmail(String to, String username, String token);
-
-    void sendPasswordResetEmail(String to, String username, String token);
-}
